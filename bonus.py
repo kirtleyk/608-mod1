db64842a31d296101c889a14a8d4e5a4e9b23eaf
Kevin Kirtley <kevin@kirtley.us>
2021-05-12 16:34:20 -0500

    Added print commands and bonus file

---
+++ bonus.py
@@ -0,0 +1,10 @@
+dataPoints = (13, 55, 155, 70, 24, 349, 29, 33, 53, 78, 25, 37)
+
+print('\nThe data points are:', dataPoints)
+
+print('\nThe min is:', min(dataPoints))
+
+print('\nThe max is:', max(dataPoints))
+
+print('\nThe range is:', min(dataPoints), '-', max(dataPoints), '\n')
+
